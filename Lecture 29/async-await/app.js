90add3817a53fcbeb5ecbf12d29bba1ad548e284
Tanisha Jain <tanishajn12@gmail.com>
2024-06-28 10:58:59 +0530

    Lecture 30

---
+++ Lecture 29/async-await/app.js
@@ -0,0 +1,34 @@
+
+
+//1. when nothing is returned from the function, 
+//by default undefined is returned
+
+
+// function something() {
+
+// }
+
+// console.log(something());
+
+//------------------------------------
+
+//on async -> it returns a promise
+// async function something() {
+//     return "tanisha"
+// }
+
+// console.log(something());
+
+
+//----------------------------------------
+// async function something() {
+//     return "tanisha"
+// }
+    
+// something().then(function(data){
+//     console.log(data);
+// });
+
+//-------------------------------------------
+
+// await -> please wait
